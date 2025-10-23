1b305fb926f18787c0389d03db98515a8ee1e395
Mio <mio@mio19.uk>
2025-10-23 23:38:24 +1100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    jovian = {
+      url = "git+https://github.com/Jovian-Experiments/Jovian-NixOS.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    chaotic.url = "git+https://github.com/chaotic-cx/nyx.git?ref=nyxpkgs-unstable";
+    rosetta-spice.url = "github:zhaofengli/rosetta-spice";
+    nixos-apple-silicon = {
+      url = "github:nix-community/nixos-apple-silicon";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
+        ];
+        flake = {
+        };
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        perSystem =
+          {
+            system,
+            ...
+          }:
+          let
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.jovian.overlays.default
+                inputs.chaotic.overlays.default
+              ];
+            };
+            lib = inputs.nixpkgs.lib;
+          in
+          {
+            packages = {
+              linuxPackages_jovian = pkgs.linuxPackages_jovian;
+            };
+          };
+      }
+    );
+}
