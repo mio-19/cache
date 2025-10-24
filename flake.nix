e352566beee50c441c3796d64f24c9cc18e17965
Mio <mio@mio19.uk>
2025-10-24 22:26:38 +1100

    up

--- flake.nix
+++ flake.nix
@@ -61,21 +61,25 @@
             lib = inputs.nixpkgs.lib;
           in
           {
-            packages = {
-              inherit (pkgs') remmina librewolf thunderbird-esr;
-              #linux_jovian = lib.mkIf (system == "x86_64-linux") pkgs.linux_jovian;
-              default = lib.mkIf (system == "x86_64-linux") (
-                pkgs.stdenv.mkDerivation rec {
-                  name = "example-package-${version}";
-                  version = "1.0";
-                  src = ./.;
-                  # cache dependencies for those packages:
-                  buildInputs = with pkgs; davinci-resolve.nativeBuildInputs;
-                  buildPhase = "echo echo Hello World > example";
-                  installPhase = "install -Dm755 example $out";
-                }
-              );
-            };
+            packages = liv.mkMerge [
+              {
+                inherit (pkgs') remmina librewolf thunderbird-esr;
+              }
+              (lib.mkIf (system == "x86_64-linux") {
+                linux_jovian = pkgs.linux_jovian;
+                default = (
+                  pkgs.stdenv.mkDerivation rec {
+                    name = "example-package-${version}";
+                    version = "1.0";
+                    src = ./.;
+                    # cache dependencies for those packages:
+                    buildInputs = with pkgs; davinci-resolve.nativeBuildInputs;
+                    buildPhase = "echo echo Hello World > example";
+                    installPhase = "install -Dm755 example $out";
+                  }
+                );
+              })
+            ];
           };
       }
     );
