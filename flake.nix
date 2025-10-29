99173dbb361982df0778a94318f9699336d3d59b
Mio <mio@mio19.uk>
2025-10-29 18:17:53 +1100

    gamescope

--- flake.nix
+++ flake.nix
@@ -115,7 +115,7 @@
                 inherit (pkgs.emacs.pkgs) magit nix-mode agda2-mode;
               }
               (lib.mkIf (pkgs.stdenv.isLinux) {
-                inherit (pkgs) totem gnome-session obsidian;
+                inherit (pkgs) totem gnome-session obsidian gamescope;
               })
               (lib.mkIf (system == "x86_64-linux") {
                 inherit (pkgs) davinci-resolve steam prusa-slicer;
