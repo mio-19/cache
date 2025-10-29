5eb841fd4c79d62a9463f8489bb6561fcf28e905
Mio <mio@mio19.uk>
2025-10-29 12:08:50 +1100

    steam

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
                 inherit (pkgs) totem gnome-session;
               })
               (lib.mkIf (system == "x86_64-linux") {
-                inherit (pkgs) davinci-resolve;
+                inherit (pkgs) davinci-resolve steam;
                 linuxv3gcc = (pkgs.linuxPackages_cachyos-gcc.cachyOverride { mArch = "GENERIC_V3"; }).kernel;
                 #linuxv4gcc = (pkgs.linuxPackages_cachyos-gcc.cachyOverride { mArch = "GENERIC_V4"; }).kernel;
                 linuxv3 = (pkgs.linuxPackages_cachyos-lto.cachyOverride { mArch = "GENERIC_V3"; }).kernel;
