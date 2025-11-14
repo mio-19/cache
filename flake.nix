8b0dabb8f088aa0a9584117c29a6a870c9241763
Mio <mio@mio19.uk>
2025-11-14 17:36:54 +1100

    don't zed

--- flake.nix
+++ flake.nix
@@ -132,7 +132,6 @@
                   inkscape
                   noto-fonts-color-emoji
                   joplin-desktop
-                  zed-editor
                   ;
                 #inherit (pkgs) thunderbird-esr; # jellyfin-media-player
                 inherit (pkgs.emacs.pkgs) magit nix-mode agda2-mode;
