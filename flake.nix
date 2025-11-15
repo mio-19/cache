ba90dee7bd19355d427947240d2adab51ca02469
Mio <mio@mio19.uk>
2025-11-15 22:40:02 +1100

    go master

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     nixpkgs-staging.url = "github:NixOS/nixpkgs/staging";
     darwin-emacs = {
       url = "github:nix-giant/nix-darwin-emacs";
