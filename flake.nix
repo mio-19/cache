da22f516944cefefdbcda2b63a5eaf70392964d9
Mio <mio@mio19.uk>
2025-11-21 20:59:01 +1100

    v4

--- flake.nix
+++ flake.nix
@@ -163,9 +163,9 @@
                   #gg
                   ;
                 linuxv3gcc = (pkgs.linuxPackages_cachyos-gcc.cachyOverride { mArch = "GENERIC_V3"; }).kernel;
-                #linuxv4gcc = (pkgs.linuxPackages_cachyos-gcc.cachyOverride { mArch = "GENERIC_V4"; }).kernel;
+                linuxv4gcc = (pkgs.linuxPackages_cachyos-gcc.cachyOverride { mArch = "GENERIC_V4"; }).kernel;
                 linuxv3 = (pkgs.linuxPackages_cachyos-lto.cachyOverride { mArch = "GENERIC_V3"; }).kernel;
-                #linuxv4 = (pkgs.linuxPackages_cachyos-lto.cachyOverride { mArch = "GENERIC_V4"; }).kernel;
+                linuxv4 = (pkgs.linuxPackages_cachyos-lto.cachyOverride { mArch = "GENERIC_V4"; }).kernel;
                 #linuxv3gcczfscachyos = linuxv3gcc.zfs_cachyos;
                 #linuxv3gccnvidiaopen = linuxv3gcc.nvidia-open;
                 #linuxv3gccnxone = linuxv3gcc.xone;
